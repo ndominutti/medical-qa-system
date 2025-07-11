b55e7303684633a7a50418e71ca31a9a77a32485
ndominutti <ndominutti@gmail.com>
2025-07-11 13:22:52 -0300

    Updated import to new naming

--- src/general_utils/__init__.py
+++ src/general_utils/__init__.py
@@ -1,4 +1,5 @@
-from config_utils import load_config
-from s3_utils import S3Manager
+from .config import load_config
+from .logging import log
+from .s3 import S3Manager
 
-__all__ = ["load_config", "S3Manager"]
+__all__ = ["load_config", "S3Manager", "log"]
